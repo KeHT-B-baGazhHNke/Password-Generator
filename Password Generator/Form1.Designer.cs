c0fa9d9ee4862f4cdf3c10c52a49f3a9e9a9587e
dr fev <chimichanga2007@mail.ru>
2024-04-14 22:58:22 +0300

    Добавьте файлы проекта.

---
+++ Password Generator/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 241);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 155);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(277, 80);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 49);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(147, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 77);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(219, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Исключить похожие символы";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 103);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(244, 20);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Включить небуквенные символы";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 129);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(182, 20);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Пароль не из rockyou.txt";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(6, 21);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(147, 22);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Длина пароля";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(336, 335);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Password Generator/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        public List<string> myList = File.ReadAllLines(@"rockyou.txt").ToList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+            int lenght = (int)numericUpDown1.Value;
+            string login = richTextBox2.Text;
+            bool similar = checkBox1.Checked;
+            bool symb = checkBox2.Checked;
+            bool rockyou = checkBox3.Checked;
+            Random rand = new Random();
+            for (int i = 0; i < lenght; i++)
+            {
+                if (similar == true && symb == true)
+                {
+                    int value = rand.Next(33, 127);
+                    while (value == 105 || value == 108 || value == 49 || value == 111 || value == 48 || value == 79)
+                    {
+                        value = rand.Next(65, 127);
+                    }
+                    richTextBox1.Text += (char)value;
+                }
+                if (similar == true && symb == false)
+                {
+                    int value = rand.Next(65, 116);
+                    if (value > 90)
+                    {
+                        value += 6;
+                    }
+                    while (value == 105 || value == 108 || value == 49 || value == 111 || value == 48 || value == 79)
+                    {
+                        value = rand.Next(65, 127);
+                    }
+                    richTextBox1.Text += (char)value;
+                }
+                if (similar == false && symb == false)
+                {
+                    int value = rand.Next(65, 116);
+                    if (value > 90)
+                    {
+                        value += 6;
+                    }
+                    richTextBox1.Text += (char)value;
+                }
+                if (similar == false && symb == true)
+                {
+                    char valuechar = (char)rand.Next(33, 127);
+                    richTextBox1.Text += valuechar.ToString();
+                }
+                if (rockyou == true)
+                {
+                    while (myList.Contains(richTextBox1.Text))
+                    {
+                        if (similar == true && symb == true)
+                        {
+                            int value = rand.Next(33, 127);
+                            while (value == 105 || value == 108 || value == 49 || value == 111 || value == 48 || value == 79)
+                            {
+                                value = rand.Next(65, 127);
+                            }
+                            richTextBox1.Text += (char)value;
+                        }
+                        if (similar == true && symb == false)
+                        {
+                            int value = rand.Next(65, 116);
+                            if (value > 90)
+                            {
+                                value += 6;
+                            }
+                            while (value == 105 || value == 108 || value == 49 || value == 111 || value == 48 || value == 79)
+                            {
+                                value = rand.Next(65, 127);
+                            }
+                            richTextBox1.Text += (char)value;
+                        }
+                        if (similar == false && symb == false)
+                        {
+                            int value = rand.Next(65, 116);
+                            if (value > 90)
+                            {
+                                value += 6;
+                            }
+                            richTextBox1.Text += (char)value;
+                        }
+                        if (similar == false && symb == true)
+                        {
+                            char valuechar = (char)rand.Next(33, 127);
+                            richTextBox1.Text += valuechar.ToString();
+                        }
+                    }
+                }
+            }
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"LoginPassword.txt");
+                sw.WriteLine($"Login: {login}");
+                sw.WriteLine($"Password: {richTextBox1.Text}");
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+    }
+}
